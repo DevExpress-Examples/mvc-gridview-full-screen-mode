f731af9bb7e67097c2c1397d55226a74647f1fd4
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/CS/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Web.Mvc;
+using CS.Models;
+
+namespace CS.Controllers {
+    public class HomeController : Controller {
+        public ActionResult Index() {
+            if(Session["TypedListModel"] == null)
+                Session["TypedListModel"] = InMemoryModel.GetTypedListModel();
+            
+            return View(Session["TypedListModel"]);
+        }
+
+        public ActionResult TypedListDataBindingPartial() {
+            return PartialView(Session["TypedListModel"]);
+        }
+    }
+}
---
+++ VB/Views/Home/Index.vbhtml
@@ -0,0 +1,17 @@
+﻿@ModelType System.Collections.IEnumerable
+<script type="text/javascript">
+    function OnInit(s, e) {
+        AdjustSize();
+        ASPxClientUtils.AttachEventToElement(window, "resize", function (evt) {
+            AdjustSize();
+        });
+    }
+    functionOnEndCallback(s, e) {
+        AdjustSize();
+    }
+    function AdjustSize() {
+        var height = document.documentElement.clientHeight;
+        grid.SetHeight(height);
+    }
+</script>
+@Html.Partial("TypedListDataBindingPartial", Model)
